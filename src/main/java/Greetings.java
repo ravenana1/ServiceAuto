9623ac9e1b694835e7dfe1394b8bbea67e93a1cc
Ana <secui.ana1@gmail.com>
2015-04-20 21:52:48 +0300

    The beginning

---
+++ src/main/java/Greetings.java
@@ -0,0 +1,23 @@
+
+public class Greetings {
+	 	private final long id;
+	    private final String content;
+	    
+	    public Greetings(){
+	    	id = 0;
+	    	content = "";
+	    };
+
+	    public Greetings(long id, String content) {
+	        this.id = id;
+	        this.content = content;
+	    }
+
+	    public long getId() {
+	        return id;
+	    }
+
+	    public String getContent() {
+	        return content;
+	    }
+}
---
+++ src/main/java/MainApp.java
@@ -0,0 +1,65 @@
+import java.sql.*;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp{
+	
+    public static void main( String[] args ){
+    	SpringApplication.run(MainApp.class, args);
+       /* try{
+            Class.forName("com.mysql.jdbc.Driver");
+        }catch (ClassNotFoundException e) {
+            System.out.println("Where is your MySQL JDBC Driver?");
+            e.printStackTrace();
+            return;
+	    }
+
+        Connection connection = null;
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Licenta2", "root", "root");
+        }catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return;
+        }
+
+        if (connection != null) {
+            System.out.println("You made it, take control your database now!");
+        } else {
+            System.out.println("Failed to make connection!");
+        }
+
+        Statement stmt = null;
+        try {
+            stmt = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        String sql;
+        sql = "INSERT into Piese (Type, Name) VALUES ('bau', 'baubau');";
+        ResultSet rs = null;
+        try {
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        sql = "SELECT id, Type, Name from Piese;";
+        try {
+            rs = stmt.executeQuery(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            while (rs.next()) {
+                System.out.println(rs.getString("Type"));
+                System.out.println(rs.getString("Name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+*/
+    }
+}
