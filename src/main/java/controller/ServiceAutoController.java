c551b460ab015a6dbc2767587d4a7fb55c85e40a
Ana <secui.ana1@gmail.com>
2015-04-23 12:45:25 +0300

    Added GetServices and GetPiese

--- src/main/java/controller/ServiceAutoController.java
+++ src/main/java/controller/ServiceAutoController.java
@@ -44,43 +44,88 @@ public class ServiceAutoController {
             e.printStackTrace();
         }
         String sql;
-        sql = "SELECT Type, Name, Time AS Duration FROM Operations;";
+        sql = "SELECT Type, Name, Time FROM Operations ORDER BY Type;";
         ResultSet result = null;
-        ResultSet urm = null;
         try {
             result = statement.executeQuery(sql);
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-
+        String currentType = "";
+        String nextType;
         List<Operation> operationsByType = new ArrayList<>();
         HashMap<String, List<Operation>> operations = new HashMap<>();
         try {
-            String currentType = result.getString("Type");
-            String nextType = result.getString("Type");
+            if(result.next()) {
+                currentType = result.getString("Type");
+                operationsByType.add(new Operation(result.getString("Name"), result.getString("Time")));
+            }
             while (result.next()) {
-
-                if(currentType == nextType){
+                nextType = result.getString("Type");
+                if(currentType.equals(nextType)){
                     operationsByType.add(new Operation(result.getString("Name"), result.getString("Time")));
                 }
                 else{
-                    operations.put(result.getString("Type"), operationsByType);
+                    operations.put(currentType, operationsByType);
                     operationsByType = new ArrayList<>();
+                    operationsByType.add(new Operation(result.getString("Name"), result.getString("Time")));
                     currentType = nextType;
                 }
-                nextType = result.getString("Type");
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        operations.put(currentType, operationsByType);
 
-        return new ServicesOffered();
+        return new ServicesOffered(operations);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/pieseOferite")
     public PieseOffered getPieseOferite() {
-        return new PieseOffered();
+
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        String sql;
+        sql = "SELECT Type, Name FROM Piese ORDER BY Type;";
+        ResultSet result = null;
+        try {
+            result = statement.executeQuery(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        String currentType = "";
+        String nextType;
+        List<String> pieseByType = new ArrayList<>();
+        HashMap<String, List<String>> piese = new HashMap<>();
+        try {
+            if(result.next()) {
+                currentType = result.getString("Type");
+                pieseByType.add(result.getString("Name"));
+            }
+            while (result.next()) {
+                nextType = result.getString("Type");
+                if(currentType.equals(nextType)){
+                    pieseByType.add(result.getString("Name"));
+                }
+                else{
+                    piese.put(currentType, pieseByType);
+                    pieseByType = new ArrayList<>();
+                    pieseByType.add(result.getString("Name"));
+                    currentType = nextType;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        piese.put(currentType, pieseByType);
+
+        return new PieseOffered(piese);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/statusMasina")
@@ -95,8 +140,40 @@ public class ServiceAutoController {
                                 @RequestParam(value = "carModel") String carModel,
                                 @RequestParam(value = "objective") String objective,
                                 @RequestParam(value = "date") String date) {
-        int number = 0;
-        return new Integer(number);
+        int registrationID = 0;
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        String sql = "INSERT INTO Clients (FirstName, LastName) VALUES (" + firstname + ", " + lastname + ");";
+        try{
+            statement.execute(sql);
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        sql = "SELECT id FROM Clients WHERE FirstName = " + firstname + " and LastName = " + lastname + ";";
+        int id;
+        ResultSet result = null;
+        try {
+            result = statement.executeQuery(sql);
+
+            if(result.next()){
+                id = result.getInt("id");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+        return registrationID;
     }
 
 }
